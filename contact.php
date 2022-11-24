628c3093314264d8a8f2025bd99538b47fc66e44
mattsan83 <cohenmatthieu1@gmail.com>
2022-11-24 11:15:09 +0100

    avancement contact.php

--- contact.php
+++ contact.php
@@ -14,13 +14,35 @@
 <body>
   <?php include_once 'assets/includes/_navbar.html' ?>
   <!-- <div class="container"> -->
-    <div class="container"><h1>Contact</h1></div>
+  <div class="container">
+    <h1 class="contact">Contact</h1>
+    <div id="form" class="">
+      <form action="" method="post">
+        <div class="input-group">
+        <input name="mail" id="mail" placeholder=" " required>
+        <label for="mail">Email</label>
+        </div>
+        <div class="input-group">
+        <input name="nom" id="name" placeholder=" " required>
+        <label for="name">Prénom et Nom</label>
+        </div>
+        <div class="input-group">
+        <input name="sujet" id="sujet" placeholder=" " required>
+        <label for="sujet">sujet</label>
+        </div>
+        <div class="input-group">
+        <input name="message" id="message" placeholder=" " required>
+        <label for="message">message</label>
+        </div>
+      </form>
+    </div>
+  </div>
   <!-- </div> -->
 
   <?php include_once 'assets/includes/_footer.html' ?>
   <!-- Javascript import files -->
 
-<script src="assets/app/index.js"></script>  
+  <script src="assets/app/index.js"></script>
 </body>
 
 </html>
