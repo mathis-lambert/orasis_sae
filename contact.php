a78d5aa46af1f810a9b23570bb69e53bda3dc193
mattsan83 <cohenmatthieu1@gmail.com>
2022-11-30 13:34:47 +0100

    contact avancement +- 80%

--- contact.php
+++ contact.php
@@ -15,28 +15,29 @@
   <?php include_once 'assets/includes/_navbar.html' ?>
   <!-- <div class="container"> -->
   <div class="container contact">
-    <h1> Contact</h1>
+    <h1 style="margin-bottom: 40px;">Contact</h1>
 
-      <form action="" method="post" class="form">
-        <div class="input-group">
+    <form action="" method="post" class="form">
+  <div class="d-flex row" style="column-gap: 4rem; flex-wrap:wrap; ">
+      <div class="input-group" id="div-mail">
         <input name="mail" id="mail" placeholder=" " required>
         <label for="mail">Email</label>
-        </div>
-        <div class="input-group">
+      </div>
+      <div class="input-group" id="div-name">
         <input name="nom" id="name" placeholder=" " required>
         <label for="name">Prénom et Nom</label>
-        </div>
-        <div class="input-group">
+      </div>
+  </div>
+      <div class="input-group" id="div-sujet">
         <input name="sujet" id="sujet" placeholder=" " required>
         <label for="sujet">sujet</label>
-        </div>
-        <div class="input-group">
-        <input name="message" id="message" placeholder=" " required>
+      </div>
+      <div class="input-group" id="div-message">
+        <textarea name="message" id="message" cols="30" rows="10" placeholder=" " required></textarea>
         <label for="message">message</label>
-        </div>
-        
-        <button type="submit" class="btn btn-blue">envoyer</button>
-      </form>
+      </div>
+      <button type="submit" class="btn btn-blue">envoyer</button>
+    </form>
   </div>
   <!-- </div> -->
 
