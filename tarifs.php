aba62b7dcdadb7c42fc78d42eccf12f1bc44364e
mattsan83 <cohenmatthieu1@gmail.com>
2023-01-03 16:11:12 +0100

    img tarif php

--- tarifs.php
+++ tarifs.php
@@ -26,15 +26,33 @@ include_once 'config/config.php';
 
     <section>
         <div class="container contact">
-            <h2 style="margin-bottom: 40px; text-align:center;">Tarifs</h2>
-            <p>Tarifs d'inscription à ORASIS 2023
-                Les frais d’inscriptions comprennent :
-                <li> participation à l'ensemble du congrès et aux pauses-cafés ;</li>
-                <li>les repas du lundi soir, mardi midi et soir, mercredi midi et soir, jeudi midi et vendredi midi ;</li>
-                <li> repas de gala du jeudi soir ;</li>
-                <li>les animations et visites (voir programme).</li>
-                Vous avez le choix entre les options* suivantes (dans la limite des places disponibles) :
-            </p>
+            <div class="content-container d-flex row">
+                <div class="main-container">
+                    <div class="para">
+                        <p>
+                        La conférence et les participants seront logés à l’établissement Miléade idéalement placé à Carqueiranne qui donne une vue sur mer.
+
+Nous proposons une formule de pension complète incluse dans les prix d’inscription et de l’hébergement.
+
+Les photos suivantes présentent les locaux et l’emplacement de cet établissement.
+                        </p>
+                    </div>
+                </div>
+                <div class="imgReveal" style="margin-bottom: 20px;">
+                    <img src="assets/img/camping.jpg" alt="" style="width: 70%;border-radius: 20px;height: 100%;">
+                </div>
+            </div>
+
+
+        <h2 style="margin-bottom: 40px; text-align:center;">Tarifs</h2>
+        <p>Tarifs d'inscription à ORASIS 2023
+            Les frais d’inscriptions comprennent :
+            <li> participation à l'ensemble du congrès et aux pauses-cafés ;</li>
+            <li>les repas du lundi soir, mardi midi et soir, mercredi midi et soir, jeudi midi et vendredi midi ;</li>
+            <li> repas de gala du jeudi soir ;</li>
+            <li>les animations et visites (voir programme).</li>
+            Vous avez le choix entre les options* suivantes (dans la limite des places disponibles) :
+        </p>
         </div>
         <div class="container-table" style="color:white; margin:auto;">
             <table style="background-color:black ;border:2px solid white; border-collapse:collapse;">
